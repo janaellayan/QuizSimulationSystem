1268153bfa38408616200ab8515086f6e3e271ef
hp <byanzkr86@gmail.com>
2025-12-31 22:41:45 +0300

    Design main form UI with custom Start button and chapter list

--- Form1/Form1/Form1.Designer.cs
+++ Form1/Form1/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.comboChapters = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(172, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your name :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.label2.Location = new System.Drawing.Point(172, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter your ID :";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.White;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(398, 158);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(239, 30);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.White;
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(398, 215);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(239, 30);
+            this.txtID.TabIndex = 3;
+            this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(90)))));
+            this.label3.Location = new System.Drawing.Point(99, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(652, 45);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Welcome! to the Quiz Simulation System";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.White;
+            this.btnStart.FlatAppearance.BorderSize = 0;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(329, 433);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(93, 35);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start Quiz";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // comboChapters
+            // 
+            this.comboChapters.FormattingEnabled = true;
+            this.comboChapters.Location = new System.Drawing.Point(398, 272);
+            this.comboChapters.Name = "comboChapters";
+            this.comboChapters.Size = new System.Drawing.Size(239, 28);
+            this.comboChapters.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
+            this.label4.Location = new System.Drawing.Point(172, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Select Chapter :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(871, 571);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboChapters);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Quiz Simulation System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox comboChapters;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1/Form1/Form1.cs
+++ Form1/Form1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,98 @@ namespace Form1
         public Form1()
         {
             InitializeComponent();
+
+            // Button basic design settings
+            btnStart.BackColor = Color.Transparent;
+            btnStart.ForeColor = Color.White;
+            btnStart.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            btnStart.Width = 130;
+            btnStart.Height = 50;
+
+            // Connect custom paint event to draw the button style
+            btnStart.Paint += btnStart_Paint;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Clear items before adding new ones
+            comboChapters.Items.Clear();
+
+            // Add chapters to the dropdown list
+            comboChapters.Items.Add("Chapter 1");
+            comboChapters.Items.Add("Chapter 2");
+            comboChapters.Items.Add("Chapter 3");
+            comboChapters.Items.Add("Chapter 4");
+            comboChapters.Items.Add("Chapter 5");
+            comboChapters.Items.Add("Chapter 6");
+            comboChapters.Items.Add("Chapter 7");
+
+            // Select the first chapter by default
+            comboChapters.SelectedIndex = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        // This function creates a rounded rectangle shape
+        private GraphicsPath GetRoundedPath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int diameter = radius * 2;
+
+            // Draw rounded corners
+            path.AddArc(rect.X, rect.Y, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Y, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - diameter, diameter, diameter, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+        // Custom drawing for the button (rounded + gradient)
+        private void btnStart_Paint(object sender, PaintEventArgs e)
+        {
+            Button btn = sender as Button;
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle rect = btn.ClientRectangle;
+
+            using (GraphicsPath path = GetRoundedPath(rect, 22))
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                rect,
+                Color.FromArgb(74, 144, 226),   // Gradient start color
+                Color.FromArgb(53, 122, 189),   // Gradient end color
+                LinearGradientMode.Horizontal))
+            {
+                // Apply rounded shape to the button
+                btn.Region = new Region(path);
+
+                // Fill the button with gradient color
+                e.Graphics.FillPath(brush, path);
+            }
+
+            // Draw button text in the center
+            TextRenderer.DrawText(
+                e.Graphics,
+                btn.Text,
+                btn.Font,
+                rect,
+                Color.White,
+                TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter
+            );
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
