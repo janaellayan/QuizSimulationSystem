63e5657cb1d13172419e6a8903dc9b672d019873
janaellayan <contactjanaellayan@gmail.com>
2026-01-02 19:24:21 +0300

    added chapters in the drop down menu in User Info Form+ fixed the properties.

--- Form1/Form1/User Info.Designer.cs
+++ Form1/Form1/User Info.Designer.cs
@@ -45,9 +45,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
-            this.label1.Location = new System.Drawing.Point(83, 78);
+            this.label1.Location = new System.Drawing.Point(74, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(208, 32);
+            this.label1.Size = new System.Drawing.Size(174, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter your name :";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -57,9 +57,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
-            this.label2.Location = new System.Drawing.Point(83, 135);
+            this.label2.Location = new System.Drawing.Point(74, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(171, 32);
+            this.label2.Size = new System.Drawing.Size(143, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter your ID :";
             // 
@@ -67,18 +67,20 @@
             // 
             this.txtName.BackColor = System.Drawing.Color.White;
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(385, 78);
+            this.txtName.Location = new System.Drawing.Point(342, 62);
+            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(267, 30);
+            this.txtName.Size = new System.Drawing.Size(238, 26);
             this.txtName.TabIndex = 2;
             // 
             // txtID
             // 
             this.txtID.BackColor = System.Drawing.Color.White;
             this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtID.Location = new System.Drawing.Point(385, 135);
+            this.txtID.Location = new System.Drawing.Point(342, 108);
+            this.txtID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(267, 30);
+            this.txtID.Size = new System.Drawing.Size(238, 26);
             this.txtID.TabIndex = 3;
             this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
             // 
@@ -87,9 +89,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Black", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(90)))));
-            this.label3.Location = new System.Drawing.Point(49, 49);
+            this.label3.Location = new System.Drawing.Point(44, 39);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(816, 54);
+            this.label3.Size = new System.Drawing.Size(689, 46);
             this.label3.TabIndex = 4;
             this.label3.Text = "Welcome! to the Quiz Simulation System";
             // 
@@ -100,9 +102,10 @@
             this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNext.ForeColor = System.Drawing.Color.White;
-            this.btnNext.Location = new System.Drawing.Point(290, 278);
+            this.btnNext.Location = new System.Drawing.Point(258, 222);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(170, 72);
+            this.btnNext.Size = new System.Drawing.Size(151, 58);
             this.btnNext.TabIndex = 5;
             this.btnNext.Text = "Next";
             this.btnNext.UseVisualStyleBackColor = false;
@@ -110,10 +113,20 @@
             // 
             // comboChapters
             // 
+            this.comboChapters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboChapters.FormattingEnabled = true;
-            this.comboChapters.Location = new System.Drawing.Point(385, 193);
+            this.comboChapters.Items.AddRange(new object[] {
+            "Chapter 1",
+            "Chapter 2",
+            "Chapter 3 ",
+            "Chapter 4 ",
+            "Chapter 5 ",
+            "Chapter 6 ",
+            "Chapter 7"});
+            this.comboChapters.Location = new System.Drawing.Point(342, 154);
+            this.comboChapters.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboChapters.Name = "comboChapters";
-            this.comboChapters.Size = new System.Drawing.Size(267, 28);
+            this.comboChapters.Size = new System.Drawing.Size(238, 24);
             this.comboChapters.TabIndex = 6;
             // 
             // label4
@@ -121,9 +134,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(60)))));
-            this.label4.Location = new System.Drawing.Point(83, 193);
+            this.label4.Location = new System.Drawing.Point(74, 154);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(186, 32);
+            this.label4.Size = new System.Drawing.Size(154, 28);
             this.label4.TabIndex = 7;
             this.label4.Text = "Select Chapter :";
             // 
@@ -138,19 +151,21 @@
             this.pnlQuestion.Controls.Add(this.txtID);
             this.pnlQuestion.Controls.Add(this.label2);
             this.pnlQuestion.Controls.Add(this.txtName);
-            this.pnlQuestion.Location = new System.Drawing.Point(58, 155);
+            this.pnlQuestion.Location = new System.Drawing.Point(52, 124);
+            this.pnlQuestion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlQuestion.Name = "pnlQuestion";
-            this.pnlQuestion.Size = new System.Drawing.Size(793, 384);
+            this.pnlQuestion.Size = new System.Drawing.Size(705, 307);
             this.pnlQuestion.TabIndex = 8;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(247)))), ((int)(((byte)(250)))));
-            this.ClientSize = new System.Drawing.Size(907, 655);
+            this.ClientSize = new System.Drawing.Size(806, 524);
             this.Controls.Add(this.pnlQuestion);
             this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Quiz Simulation System(User Info)";
             this.Load += new System.EventHandler(this.Form1_Load);
