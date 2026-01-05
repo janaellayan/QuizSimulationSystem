f6c89c49a975ba3daefa5d2edad6df3b57c51d3c
janaellayan <contactjanaellayan@gmail.com>
2026-01-05 22:20:45 +0300

    added a question class to fetch questions later on according to chapter

---
+++ Form1/Form1/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form1.Models
+{
+
+    public enum QuestionType
+    {
+        MultipleChoice,
+        TrueFalse,
+    }
+    // to classify the questions in our lists 
+    public class Question
+    {
+        public string Text { get; set; }
+        public QuestionType Type { get; set; }
+        public string[] Options { get; set; }
+        public int CorrectIndex { get; set; }
+    }
+}
