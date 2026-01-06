81b6575abcae392eeccf83dfbdbcad7fe83306d9
janaellayan <contactjanaellayan@gmail.com>
2026-01-06 09:45:12 +0300

    added the discussed label this is to allow the possibility of increasing the scope later on. this program is made for students only right now, but if further development took place it could be expanded for instructors as well.

--- Form1/Form1/Result_Form.Designer.cs
+++ Form1/Form1/Result_Form.Designer.cs
@@ -34,8 +34,9 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel = new System.Windows.Forms.LinkLabel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,9 +44,9 @@
             // 
             this.lblTime.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblTime.Location = new System.Drawing.Point(173, 225);
+            this.lblTime.Location = new System.Drawing.Point(154, 180);
             this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(184, 40);
+            this.lblTime.Size = new System.Drawing.Size(164, 32);
             this.lblTime.TabIndex = 4;
             this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -53,9 +54,9 @@
             // 
             this.lblScore.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblScore.Location = new System.Drawing.Point(170, 151);
+            this.lblScore.Location = new System.Drawing.Point(151, 121);
             this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(187, 40);
+            this.lblScore.Size = new System.Drawing.Size(166, 32);
             this.lblScore.TabIndex = 5;
             this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -64,9 +65,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI Black", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(90)))));
-            this.label3.Location = new System.Drawing.Point(92, 28);
+            this.label3.Location = new System.Drawing.Point(82, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(255, 54);
+            this.label3.Size = new System.Drawing.Size(215, 46);
             this.label3.TabIndex = 6;
             this.label3.Text = "Quiz Result ";
             // 
@@ -77,9 +78,10 @@
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.Color.White;
-            this.btnExit.Location = new System.Drawing.Point(501, 434);
+            this.btnExit.Location = new System.Drawing.Point(445, 347);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(170, 72);
+            this.btnExit.Size = new System.Drawing.Size(151, 58);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -92,9 +94,10 @@
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBack.ForeColor = System.Drawing.Color.White;
-            this.btnBack.Location = new System.Drawing.Point(225, 434);
+            this.btnBack.Location = new System.Drawing.Point(200, 347);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(170, 72);
+            this.btnBack.Size = new System.Drawing.Size(151, 58);
             this.btnBack.TabIndex = 8;
             this.btnBack.Text = "Back to Main";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -108,47 +111,63 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.lblScore);
             this.panel1.Controls.Add(this.lblTime);
-            this.panel1.Location = new System.Drawing.Point(225, 56);
+            this.panel1.Location = new System.Drawing.Point(200, 45);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(446, 341);
+            this.panel1.Size = new System.Drawing.Size(396, 273);
             this.panel1.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
-            this.label1.Location = new System.Drawing.Point(65, 229);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 30);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Time :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
-            this.label2.Location = new System.Drawing.Point(65, 155);
+            this.label2.Location = new System.Drawing.Point(58, 124);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 30);
+            this.label2.Size = new System.Drawing.Size(69, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "Score :";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.label1.Location = new System.Drawing.Point(58, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Time :";
+            // 
+            // linkLabel
+            // 
+            this.linkLabel.AutoSize = true;
+            this.linkLabel.LinkColor = System.Drawing.Color.RoyalBlue;
+            this.linkLabel.Location = new System.Drawing.Point(260, 441);
+            this.linkLabel.Name = "linkLabel";
+            this.linkLabel.Size = new System.Drawing.Size(300, 16);
+            this.linkLabel.TabIndex = 10;
+            this.linkLabel.TabStop = true;
+            this.linkLabel.Text = "Are you an instructor? Contact us for any requests.";
+            this.linkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel_LinkClicked);
+            // 
             // Result_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(871, 624);
+            this.ClientSize = new System.Drawing.Size(774, 499);
+            this.Controls.Add(this.linkLabel);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnExit);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Result_Form";
             this.Text = "Result_Form";
             this.Load += new System.EventHandler(this.Result_Form_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -162,5 +181,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel;
     }
 }
